dbddd114380ae751323c56c81336b96cef144c08
J1w0n9 <jjw090910@naver.com>
2025-08-06 22:17:14 +0900

    Update 별 찍기 － 12.c

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++){
         for (int j = 0; j < n - i - 1; j++){
             printf(" ");
         }
@@ -22,4 +22,5 @@ int main() {
         printf("\n");
     }
     return 0;
+
 }
