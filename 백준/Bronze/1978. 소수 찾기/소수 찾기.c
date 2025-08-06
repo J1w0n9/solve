52c860fd2f0f48a15f12ac127bc170d4ad032324
J1w0n9 <jjw090910@naver.com>
2025-08-06 21:28:10 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int is_prime(int num){
+    if (num < 2) return 0;
+    for (int i = 2; i * i <= num; i++){
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+int main(){
+    int N, num, count = 0;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++){
+        scanf("%d", &num);
+        if (is_prime(num)){
+            count++;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
