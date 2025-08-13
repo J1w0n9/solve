fa732dccd408c4eb5e847cd7dabce19f21bc9964
J1w0n9 <jjw090910@naver.com>
2025-08-13 23:23:45 +0900

    Update 중복된 숫자.c

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.c
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     long long N;
     scanf("%lld", &N);
     long long sum = 0;
     long long total = (N - 1) * N / 2;
     long long x;
-    for(long long i = 0; i < N; i++) {
+    for(long long i = 0; i < N; i++){
         scanf("%lld", &x);
         sum += x;
     }
