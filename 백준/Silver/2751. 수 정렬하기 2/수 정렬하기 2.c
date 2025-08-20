f6008f81852e4d4add80219d7563bc4862978fad
J1w0n9 <jjw090910@naver.com>
2025-08-20 21:55:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 312 ms, Memory: 8804 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main(){
-    int n, x;
-    int count[2000001] = {0};
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++){
-        scanf("%d", &x);
-        count[x + 1000000] = 1;
+int main() {
+    int a, b;
+    scanf("%d\n", &a);
+    int arr[2000001] = {0};
+    for(int i = 0; i<a; i++){
+        scanf("%d\n", &b);
+        arr[b+1000000] = 1;
     }
-    for (int i = 0; i <= 2000000; i++){
-        if (count[i]) {
+    for(int i = 0; i <= 2000000; i++){
+        if(arr[i]){
             printf("%d\n", i - 1000000);
         }
     }
