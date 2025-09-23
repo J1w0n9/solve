1321e65b15b1baf1e6d6cfc5d51bf153e7f4e27b
J1w0n9 <jjw090910@naver.com>
2025-09-23 23:35:57 +0900

    Update 초콜릿 자르기.c

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.c
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int N, M;
-    scanf("%d %d", &N, &M);
-    printf("%d\n", N*M -1);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    printf("%d\n", n*m -1);
     return 0;
+
 }
